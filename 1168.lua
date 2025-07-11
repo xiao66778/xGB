ccb9c4ec673773f1a0a4d0f3e610d6dcd3f640a3
xiao66778 <kghq006@gmail.com>
2025-07-11 22:13:57 +0800

    更新 1168.lua

--- 1168.lua
+++ 1168.lua
@@ -27,7 +27,7 @@ Window:EditOpenButton({
     Title = "小空脚本",
     Icon = "monitor",
     CornerRadius = UDim.new(0,60),
-    StrokeThickness = 6,
+    StrokeThickness = 5，
     Color = ColorSequence.new( -- gradient
         Color3.fromHex("6E94FF"), 
         Color3.fromHex("73E8FF"),
