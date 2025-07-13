66347c117c4ae4ff1efe4153a3684f783e784688
xiao66778 <kghq006@gmail.com>
2025-07-13 14:13:07 +0800

    更新 1168.lua

--- 1168.lua
+++ 1168.lua
@@ -2,7 +2,7 @@ local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/xiao66
 
 local Window = WindUI:CreateWindow({
     Title = "小空脚本",
-    Icon = "verified",
+    Icon = "verified-check",
     Author = "作者:晓空",
     Folder = "CloudHub",
     Size = UDim2.fromOffset(400, 300),
